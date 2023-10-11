cc443f20797361451894d53fad877f51947d12cf
BAHA Ephraim <jeansamist@gmail.com>
2023-10-11 16:27:39 +0100

    ***

--- src/components/balls.tsx
+++ src/components/balls.tsx
@@ -16,24 +16,24 @@ export const Balls: FunctionComponent = () => {
         <div
           className="w-40 h-40 bg-violet-600 rounded-full absolute translate-x-2/3 -translate-y-1/2"
           style={{
-            top: `${mouse.y}px`,
-            left: `${mouse.x}px`,
+            top: `${mouse.y + Math.random() * 50}px`,
+            left: `${mouse.x + Math.random() * 50}px`,
             transition: "top 0.5s linear, left 0.1s linear",
           }}
         ></div>
         <div
           className="w-64 h-64 bg-violet-800 rounded-full absolute -translate-x-1/4 -translate-y-1/4"
           style={{
-            top: `${mouse.y - 100}px`,
-            left: `${mouse.x - 200}px`,
+            top: `${mouse.y + Math.random() * 50 - 100}px`,
+            left: `${mouse.x + Math.random() * 50 - 200}px`,
             transition: "top 1s linear, left 0.3s linear",
           }}
         ></div>
         <div
           className="w-60 h-60 bg-violet-500 rounded-full absolute -translate-x-2/3 translate-y-1/2"
           style={{
-            top: `${mouse.y + 100}px`,
-            left: `${mouse.x + 100}px`,
+            top: `${mouse.y + Math.random() * 50 + 100}px`,
+            left: `${mouse.x + Math.random() * 50 + 100}px`,
             transition: "top 1.5s linear, left 0.5s linear",
           }}
         ></div>
