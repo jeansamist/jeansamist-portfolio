f1cd422675caa394a3de4d41f47c3f46007b6590
BAHA Ephraim <jeansamist@gmail.com>
2024-07-27 23:23:00 +0100

    resetting the project with a new version of next

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,20 +7,7 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
-      colors: {
-        primary: "hsl(15, 67%, 52%)",
-        gunmetal: "hsl(216, 18%, 16%)",
-        silver: "hsl(216, 4%, 75%)",
-        onyx: "hsl(217, 10%, 25%)",
-      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
